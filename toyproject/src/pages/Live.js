4fe8b2dc25d78dce59fbb4089005a81dc496e0d3
백승훈 <khbak111@naver.com>
2024-10-08 15:10:35 +0900

    1008 백승훈: live, useInput, custom input추가

---
+++ toyproject/src/pages/Live.js
@@ -0,0 +1,12 @@
+import CreateLive from "../components/live/CreateLive";
+
+function Live() {
+  return (
+    <div>
+      <h1>Live Session</h1>
+      <CreateLive />
+    </div>
+  );
+}
+
+export default Live;
