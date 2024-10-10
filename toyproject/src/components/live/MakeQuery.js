db616133ea31613111ffdd106a8bbb249b3cfe14
백승훈 <khbak111@naver.com>
2024-10-10 16:24:10 +0900

    1010승훈: 쿼리생성 로직 추가

--- toyproject/src/components/live/MakeQuery.js
+++ toyproject/src/components/live/MakeQuery.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import useInput from "../../hooks/useInput";
 import Input from "../common/Input";
 import { queryToggle } from "../../pages/Live";
@@ -10,9 +10,40 @@ function MakeQuery() {
   const min = useInput();
   const max = useInput();
 
+  const [queryList, setQueryList] = useState([]);
+  const [selectedQuery, setSelectedQeury] = useState([]);
+
+  useEffect(() => {
+    // 쿼리를 만들러 들어왔을 때 처음 fetch 하는 effect
+    const queries = [];
+    for (let i = 0; i < 3; i++) {
+      queries.push({ id: "q" + i, query: "query" + i });
+    }
+    setQueryList(queries);
+  }, []);
+
+  const onQueryClick = (e) => {
+    setSelectedQeury([...selectedQuery, { id: e.target.innerText, query: e.target.innerText }]);
+  };
+
+  const onDisSelectQuery = (e) => {
+    const newQueries = [];
+    selectedQuery.forEach((item) => {
+      if (item.id !== e.target.innerText) {
+        newQueries.push(item);
+      }
+    });
+
+    setSelectedQeury(newQueries);
+    console.log(newQueries);
+  };
+
+  // 쿼리 만들기를 눌렀을 때 작동하는 부분
   const onMakeClick = () => {
     setQtoggle(false);
   };
+
+  // 취소하기 눌렀을 때 작동하는 부분
   const onCancel = () => {
     setQtoggle(false);
   };
@@ -26,21 +57,31 @@ function MakeQuery() {
         </div>
 
         <div>
-          <span>태그 선택</span>
-          <ul>
-            {/* 여기에 선택된 태그가 li 형태로 들어옴 */}
-            {/* 클릭하면 없어지는 기능까지 추가 */}
-          </ul>
+          <span>선택된 태그</span>
+          <div>
+            <ul>
+              {/* 여기에 선택된 태그가 들어옴 */}
+              {selectedQuery.map((item) => (
+                <li key={item.id} onClick={onDisSelectQuery}>
+                  {item.query}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
         <div>
-          <ul>
-            {/* 여기에 태그 종류 들어옴 */}
-            <li>태그</li>
-            <li>태그</li>
-            <li>태그</li>
-            <li>...</li>
-          </ul>
+          <span>쿼리 종류</span>
+          <div>
+            <ul>
+              {/* 여기에 태그 종류 들어옴 */}
+              {queryList.map((tag) => (
+                <li key={tag.id} onClick={onQueryClick}>
+                  {tag.query}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
         <div>
