de711415eb476d3d3d2162e301516d008504033c
Seunghun <126738939+Shbak111@users.noreply.github.com>
2024-10-12 15:09:14 +0900

    세션 관련 파일구조 생성 (#6)
    * 1012승훈: session 기본 설정 추가
    
    * 1012승훈: fix

--- toyproject/src/index.js
+++ toyproject/src/index.js
@@ -6,11 +6,14 @@ import Live from "./pages/Live";
 import Study from "./pages/Study";
 import CreateStudy from "./components/studymain/CreateStudy";
 import GlobalStyle from "./styles/GlobalStyle";
-import CreateLive from "./components/live/CreateLive";
+import CreateLive from "./components/live/create/CreateLive";
 import Main from "./pages/Main";
 import List from "./pages/List";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Session from "./pages/Session";
+import SessionWait from "./components/live/session/SessionWait";
+import SessionProgress from "./components/live/session/SessionProgress";
 
 const router = createBrowserRouter([
   {
@@ -28,12 +31,12 @@ const router = createBrowserRouter([
     ],
   },
   {
-      path: "/study",
-      element: <Study />,
+    path: "/study",
+    element: <Study />,
   },
   {
-      path: "/create-study",
-      element: <CreateStudy />,
+    path: "/create-study",
+    element: <CreateStudy />,
   },
   {
     path: "/main",
@@ -50,6 +53,20 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <Signup />,
+  },
+  {
+    path: "/session/:session_id/:user_id",
+    element: <Session />,
+    children: [
+      {
+        path: "wait",
+        element: <SessionWait />,
+      },
+      {
+        path: "progress",
+        element: <SessionProgress />,
+      },
+    ],
   },
 ]);
 
