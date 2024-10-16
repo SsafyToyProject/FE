4301ed46d65d4931e0d5ab4b79c9307f055462e6
백승훈 <khbak111@naver.com>
2024-10-16 15:06:57 +0900

    add: 1016승훈 쿼리추가 성공

--- toyproject/src/components/live/create/MakeQuery.js
+++ toyproject/src/components/live/create/MakeQuery.js
@@ -1,4 +1,5 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
+import axios from "axios";
 import useInput from "../../../hooks/useInput";
 import { queryToggle } from "../../../pages/Live";
 import {
@@ -12,6 +13,9 @@ import {
   CancelButton,
   CustomInput,
   QueryListContainer,
+  RangeInputWrapper,
+  RangeInput,
+  InputLabel,
 } from "../../../styles/live_styles/MakeQueryStyles";
 
 function MakeQuery() {
@@ -32,29 +36,102 @@ function MakeQuery() {
     "sorting",
     "binary_search",
   ]);
-  const [selectedQuery, setSelectedQeury] = useState([]);
+  const [plusQuery, setPlusQuery] = useState([]);
+  const [minusQuery, setMinusQuery] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onQueryClick = (e) => {
-    const exists = selectedQuery.some((item) => item === e.target.innerText);
+  const onPlusQueryClick = (e) => {
+    const exists = plusQuery.some((item) => item === e.target.innerText);
 
     if (!exists) {
-      setSelectedQeury([...selectedQuery, e.target.innerText]);
+      setPlusQuery([...plusQuery, e.target.innerText]);
     }
   };
 
-  const onDisSelectQuery = (e) => {
+  const onMinusQueryClick = (e) => {
+    const exists = minusQuery.some((item) => item === e.target.innerText);
+
+    if (!exists) {
+      setMinusQuery([...minusQuery, e.target.innerText]);
+    }
+  };
+
+  const onDisSelectPlusQuery = (e) => {
     const newQueries = [];
-    selectedQuery.forEach((item) => {
+    plusQuery.forEach((item) => {
       if (item !== e.target.innerText) {
         newQueries.push(item);
       }
     });
 
-    setSelectedQeury(newQueries);
+    setPlusQuery(newQueries);
   };
 
-  const onMakeClick = () => {
-    setQtoggle(false);
+  const onDisSelectMinusQuery = (e) => {
+    const newQueries = [];
+    minusQuery.forEach((item) => {
+      if (item !== e.target.innerText) {
+        newQueries.push(item);
+      }
+    });
+
+    setMinusQuery(newQueries);
+  };
+
+  // 만들기 클릭 => 이거 쿼리등록 엄청 오래걸려서 이거 하는 동안 로딩이 보이게끔 수정해야 함...
+  const onMakeClick = async () => {
+    // 스트링 만들기
+    let queryStr = "";
+    if (max.value !== "") {
+      if (queryStr.length === 0) {
+        queryStr += "s%23" + max.value + "..";
+      } else {
+        queryStr += "+s%23" + max.value + "..";
+      }
+    }
+    // 추가 쿼리 스트링에 넣기
+    plusQuery.forEach((item, idx) => {
+      if (queryStr.length === 0) {
+        queryStr += "%23" + item;
+      } else {
+        queryStr += "+" + "%23" + item;
+      }
+    });
+    // 제외 쿼리 스트링에 넣기
+    minusQuery.forEach((item, idx) => {
+      if (queryStr.length === 0) {
+        queryStr += "-%23" + item;
+      } else {
+        queryStr += "+" + "-%23" + item;
+      }
+    });
+    // 사람 수 범위 스트링에 넣기
+    if (min.value !== "") {
+      if (queryStr.length === 0) {
+        queryStr += "s%23.." + min.value;
+      } else {
+        queryStr += "+s%23.." + min.value;
+      }
+    }
+
+    console.log(queryStr);
+
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post("/crawl/query", {
+        title: queryName.value,
+        query_str: queryStr,
+      });
+
+      console.log("쿼리 등록 성공! " + response);
+
+      //setQtoggle(false);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const onCancel = () => {
@@ -69,11 +146,11 @@ function MakeQuery() {
       </Section>
 
       <Section>
-        <Label>선택된 태그</Label>
-        <QueryListContainer $isEmpty={selectedQuery.length === 0}>
+        <Label>추가 선택된 태그</Label>
+        <QueryListContainer $isEmpty={plusQuery.length === 0}>
           <QueryList>
-            {selectedQuery.map((item, idx) => (
-              <QueryItem key={idx} onClick={onDisSelectQuery}>
+            {plusQuery.map((item, idx) => (
+              <QueryItem key={idx} onClick={onDisSelectPlusQuery}>
                 {item}
               </QueryItem>
             ))}
@@ -82,10 +159,23 @@ function MakeQuery() {
       </Section>
 
       <Section>
-        <Label>쿼리 종류</Label>
+        <Label>제외 선택된 태그</Label>
+        <QueryListContainer $isEmpty={minusQuery.length === 0}>
+          <QueryList>
+            {minusQuery.map((item, idx) => (
+              <QueryItem key={idx} onClick={onDisSelectMinusQuery}>
+                {item}
+              </QueryItem>
+            ))}
+          </QueryList>
+        </QueryListContainer>
+      </Section>
+
+      <Section>
+        <Label>추가 쿼리</Label>
         <QueryList>
           {queryList.map((tag, idx) => (
-            <QueryItem key={idx} onClick={onQueryClick}>
+            <QueryItem key={idx} onClick={onPlusQueryClick}>
               {tag}
             </QueryItem>
           ))}
@@ -93,17 +183,38 @@ function MakeQuery() {
       </Section>
 
       <Section>
-        <Label>문제 푼 사람 수 범위 선택</Label>
+        <Label>제외 쿼리</Label>
+        <QueryList>
+          {queryList.map((tag, idx) => (
+            <QueryItem key={idx} onClick={onMinusQueryClick}>
+              {tag}
+            </QueryItem>
+          ))}
+        </QueryList>
+      </Section>
+
+      <Section>
+        <Label>문제 푼 사람 수 범위 입력</Label>
+
         <RangeContainer>
-          <CustomInput name="min" {...min} />
-          <span>~</span>
-          <CustomInput name="max" {...max} />
+          <RangeInputWrapper>
+            <RangeInput name="max" {...max} />
+            <InputLabel>이상</InputLabel>
+          </RangeInputWrapper>
+        </RangeContainer>
+
+        <RangeContainer>
+          <RangeInputWrapper>
+            <RangeInput name="min" {...min} />
+            <InputLabel>이하</InputLabel>
+          </RangeInputWrapper>
         </RangeContainer>
       </Section>
 
       <div>
         <Button onClick={onMakeClick}>쿼리 생성하기</Button>
         <CancelButton onClick={onCancel}>취소</CancelButton>
+        {isLoading && <span>로딩중입니다...</span>}
       </div>
     </Container>
   );
