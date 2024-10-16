e3c4f21920538c154b2cfbac1ae78ce765ec2634
3957ki <3957ki@gmail.com>
2024-10-16 22:47:43 +0900

    3957ki: 세션 대기화면 통신테스트 완료

--- toyproject/src/components/live/session/SessionWait.js
+++ toyproject/src/components/live/session/SessionWait.js
@@ -1,5 +1,5 @@
-import { useState, useEffect, useRef } from "react";
-import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import useInterval from "../../../hooks/useInterval";
 import axios from "axios";
 import dayjs from "dayjs";
@@ -16,68 +16,68 @@ import {
 } from "../../../styles/sessionWait_styles/CreateSessionWaitStyles";
 
 function SessionWait() {
-  /*
-SessionWait에서 필요한 것: session_id, user_id (context로 부터 가져오기)
-5분전에 가져올 수 있는 정보: (problem_id, difficulty, title) 문제정보
-prop으로 progress에 넘겨 줘야 할것: problem list
+  const [now, setNow] = useState(dayjs()); // 현재 시간 상태
+  const [participants, setParticipants] = useState([]); // 참가자 정보
+  const [problems, setProblems] = useState([]); // 문제 정보
 
-문제 정보조회 fetch url: /crawl/problem/{problemID}
-요청 파라미터: X
-답변 파라미터: 
-{
-    "problem_id":int,
-    "difficulty":int,
-    "title":string,
-}
-
-*/
-  const [now, setNow] = useState(1);
-  const [participants, setParticipants] = useState([]);
-  const [problems, setProblems] = useState([]);
-
-  // props는 세션 정보
+  const user_id = 3; // 임시
   const navigate = useNavigate();
   const location = useLocation();
-  const props = location.state;
+  // const props = location.state;
 
-  // 문제 정보 GET 요청
-  const fetchProblems = async () => {
+  // 임시로 props 설정
+  const props = {
+    session_id: 1,
+    start_at: "2024-10-16 22:45:00.0",
+    end_at: "2024-10-17 04:38:00.0",
+    problemCount: 4,
+  };
+
+  // 제한 시간 구하기
+  const timeLimit = dayjs(props.end_at).diff(props.start_at, "m");
+  const day = Math.floor(timeLimit / 60 / 24);
+  const hour = Math.floor(timeLimit / 60) % 24;
+  const minute = timeLimit % 60;
+  const start_at = dayjs(props.start_at).format("YYYY.MM.DD HH:mm:ss");
+
+  // 문제 정보, 참가자 정보 GET 요청 함수
+  const fetchInfo = async () => {
     try {
+      // 세션 아이디로 문제 id 가져오기
+      const response = await axios.get(`/session/${props.session_id}`);
+
+      // 각 문제 아이디별 상세 정보 한 번에 가져오기
+      const problemRequests = response.data.session_problems.map((current) =>
+        axios.get(`/crawl/problem/${current.problem_id}`)
+      );
+
+      // 모든 요청이 완료되면 상태 업데이트
+      const problemResponses = await Promise.all(problemRequests);
+      setProblems(problemResponses.map((res) => res.data));
+
+      // 참가자 정보 저장하기
+      setParticipants(response.data.session_participants);
     } catch (error) {
       console.error("문제 정보를 불러오는 중 에러 발생:", error);
     }
   };
 
-  // 참가자 정보 GET 요청
-  const fetchSession = async () => {
-    try {
-    } catch (error) {
-      console.error("문제 정보를 불러오는 중 에러 발생:", error);
-    }
-  };
-
-  // 1초마다 현재시간 체크
+  // 1초마다 현재 시간 체크
   useInterval(() => {
-    setNow(dayjs().diff(dayjs("2021-10-11 10:30:25.495", "YYYY-MM-DD HH:mm:ss.SSS"), "s"));
-    // 3분 차이나면 문제 정보 요청
-    fetchProblems();
-    // 0초 차이나면 progress로 이동
-    fetchSession();
+    setNow(dayjs()); // 현재 시간 갱신
+    const timeDiff = dayjs(props.start_at).diff(now, "s");
+
+    // 세션 시작 3분 전이면 문제 정보 요청 (임시로 10초 전으로 설정)
+    if (timeDiff === 10) {
+      fetchInfo();
+    }
+
+    // 세션 시작 시간이 되면 progress 화면으로 이동
+    if (timeDiff === 0) {
+      console.log(problems);
+      navigate(`/session/${props.session_id}/${user_id}/progress`, { state: problems });
+    }
   }, 1000);
-
-  // 시작 5분 전에 문제 정보를 받아올 것 / 참가자 정보도 받아올 것
-  // useEffect(() => {
-  //   const fetchProblems = async () => {
-  //     try {
-  //     } catch (error) {
-  //       console.error("문제 정보를 불러오는 중 에러 발생:", error);
-  //     }
-  //   };
-
-  //   fetchProblems();
-  // }, []);
-
-  // 시작 시간이 되면 progress화면으로 이동, session_id와 user_id를 param으로 넘기고, 세션 정보를 prop으로 넘기기
 
   return (
     <>
@@ -85,22 +85,24 @@ prop으로 progress에 넘겨 줘야 할것: problem list
       <Container>
         <InfoBox>
           <LeftBox>
-            <Title>시작시간</Title>
-            <Time>{/*props.startTime*/}</Time>
+            <Title>시작일시</Title>
+            <Time>{start_at}</Time>
           </LeftBox>
           <Title>/</Title>
           <RightBox>
             <Title>제한시간</Title>
-            <Time>{/*props.timeLimit*/}</Time>
+            <Time>
+              {day}일 {hour}시간 {minute}분
+            </Time>
           </RightBox>
         </InfoBox>
         <DetailBox>
-          {/* 참가자 정보는 5분전 혹은 실시간으로 세션id로 요청하기 */}
-          <DetailItem>참가자 : {participants.map((p) => p + ", ")}</DetailItem>
-          <DetailItem>문제 수: {/*props.problemCount*/}개</DetailItem>
+          <DetailItem>참가자 : {participants.join(", ")}</DetailItem>
+          <DetailItem>문제 수: {props.problemCount}개</DetailItem>
         </DetailBox>
       </Container>
     </>
   );
 }
+
 export default SessionWait;
